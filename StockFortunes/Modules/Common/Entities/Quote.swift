649c8c45fd02d03e8ae3a071b52ff513227f52e7
Schenkeman <schenkeman@gmail.com>
2021-06-27 16:32:06 +0300

    new quote model is implemented

--- StockFortunes/Modules/Common/Entities/Quote.swift
+++ StockFortunes/Modules/Common/Entities/Quote.swift
@@ -4,23 +4,73 @@
 //
 //  Created by Vladislav Shinkevich on 6/27/21.
 //
-
 import Foundation
 
-struct Quote {
+struct QuoteResponseModel: Codable {
+    let quoteResponse: QuoteResponse
+}
 
-    var quote: String?
-    var character: String?
-    var image: String?
-    var characterDirection: String?
+// MARK: - regularmarketopenQuoteResponse
+struct QuoteResponse: Codable {
+    let result: [Quote]
+    let error: JSONNull?
+}
 
-//    init?(map: Map) {}
-//
-//    mutating func mapping(map: Map) {
-//        quote              <- map["quote"]
-//        character          <- map["character"]
-//        image              <- map["image"]
-//        characterDirection <- map["characterDirection"]
-//    }
+struct Quote: Codable {
 
+    var ticker: String
+    var title: String
+
+    var currentPrice: Double
+    var diffPrice: Double
+    var changePercent: Double
+
+    var favourite: Bool = false
+    var timeAddedToFavourite: Date?
+
+}
+
+extension Quote {
+    enum QuoteCodingKeys: String, CodingKey {
+        case ticker = "symbol"
+        case title = "shortName"
+
+        case currentPrice = "regularMarketPrice"
+        case diffPrice = "regularMarketChange"
+        case changePercent = "regularMarketChangePercent"
+    }
+
+    init(from decoder: Decoder) throws {
+        let conatiner = try decoder.container(keyedBy: QuoteCodingKeys.self)
+        ticker = try conatiner.decodeWrapper(key: .ticker, defaultValue: "No data")
+        title = try conatiner.decodeWrapper(key: .title, defaultValue: "No data")
+        currentPrice = try conatiner.decodeWrapper(key: .currentPrice, defaultValue: 0.00)
+        diffPrice = try conatiner.decodeWrapper(key: .diffPrice, defaultValue: 0.00)
+        changePercent = try conatiner.decodeWrapper(key: .changePercent, defaultValue: 0.00)
+    }
+}
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
 }
